00fbbd4e003e5b35bface8c055e8918198f41793
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-02-26 16:06:25 -0300

    feat: utilizando a lib react-query inves do custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import { useFetch } from './hooks/useFetch';
 
 type Repos = {
@@ -6,14 +8,21 @@ type Repos = {
 }
 
 function App() {
-  const { data: repositories, isFetching, error } = useFetch<Repos[]>(
-    "https://api.github.com/users/markus-del/repos"
-  );
+  // const { data: repositories, isFetching, error } = useFetch<Repos[]>(
+  //   "https://api.github.com/users/markus-del/repos"
+  // );
+
+  const { data: repositories, isFetching } = useQuery<Repos[]>('repos', async () => {
+    const response = await axios.get('https://api.github.com/users/markus-del/repos');
+    return response.data;
+  }, {
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <ul>
       {isFetching && <p>Carregando ...</p>}
-      {error && <p>{error.message}</p>}
+      {/* {error && <p>{error.message}</p>} */}
       {repositories?.map(repo => {
         return (
           <li key={repo.full_name}>
